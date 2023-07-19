4c52759a43aca4ea493add0caa73ef06dad8f17c
Dylan Littlewood <dylanlittlewood@live.co.uk>
2023-07-19 23:33:58 +0100

    more style changes

--- src/components/navigation/sidebar.tsx
+++ src/components/navigation/sidebar.tsx
@@ -1,12 +1,15 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { Button } from "../ui/button";
-import { CalendarClock, LayoutDashboard } from "lucide-react";
+import { CalendarClock, ChevronLeft, ChevronRight, LayoutDashboard } from "lucide-react";
+import { useState } from "react";
 
 export default function Sidebar() {
+  const [isOpen, setIsOpen] = useState(true);
   return (
-    <div className="flex flex-col gap-4 items-center p-4">
-          <Link to={'/Dashboard'}><Button variant={'ghost'} size={'sq'}><LayoutDashboard className="h-8 w-8" /></Button></Link>
-          <Link to={'/Schedule'}><Button variant={'ghost'} size={'sq'}><CalendarClock className="h-8 w-8" /></Button></Link>
-        </div>
+    <div className="flex flex-col gap-4 p-4">
+      <Button variant={'ghost'} size={'sq'} align={'start'} onClick={() => { setIsOpen(!isOpen) } }>{isOpen ? <ChevronLeft className="h-8 w-8" /> : <ChevronRight className="h-8 w-8" />}{isOpen && <h2 className="ml-4">Close</h2>}</Button>
+      <NavLink to={'/Dashboard'} className={({isActive}) => isActive ? "w-full bg-primary/10 rounded-lg" : "w-full"}><Button variant={'ghost'} size={'sq'} align={'start'}><LayoutDashboard className="h-8 w-8" />{isOpen && <h2 className="ml-4">Dashboard</h2>}</Button></NavLink>
+      <NavLink to={'/Schedule'} className={({isActive}) => isActive ? "w-full bg-primary/10 rounded-lg" : "w-full"}><Button variant={'ghost'} size={'sq'} align={'start'}><CalendarClock className="h-8 w-8" />{isOpen && <h2 className="ml-4">Schedule</h2>}</Button></NavLink>
+    </div>
   )
 }
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -4,7 +4,7 @@ import { VariantProps, cva } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background",
+  "inline-flex items-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background",
   {
     variants: {
       variant: {
@@ -22,12 +22,19 @@ const buttonVariants = cva(
         default: "h-10 py-2 px-4",
         sm: "h-9 px-3 rounded-md",
         lg: "h-11 px-8 rounded-md",
-        sq: "h-14 px-4 py-4",
+        sq: "h-14 px-4 py-4 w-full",
       },
+      align: {
+        default: "justify-center",
+        start: "justify-start",
+        end: "justify-end",
+        centre: "justify-center",
+      }
     },
     defaultVariants: {
       variant: "default",
       size: "default",
+      align:"default",
     },
   }
 )
@@ -37,10 +44,10 @@ export interface ButtonProps
     VariantProps<typeof buttonVariants> {}
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, ...props }, ref) => {
+  ({ className, variant, size, align, ...props }, ref) => {
     return (
       <button
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(buttonVariants({ variant, size, align, className }))}
         ref={ref}
         {...props}
       />
