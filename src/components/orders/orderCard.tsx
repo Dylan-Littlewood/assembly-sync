5405cfcfd48fc112f768a5303c3a358dc5b348b3
Dylan Littlewood <dylanlittlewood@live.co.uk>
2023-07-24 12:52:31 +0100

    Set up basic scheduling

--- src/components/orders/orderCard.tsx
+++ src/components/orders/orderCard.tsx
@@ -9,6 +9,11 @@ import {
 } from "@/components/ui/card"
 import { Badge } from "../ui/badge"
 import { Order } from "@/lib/types"
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { Calendar } from "../ui/calendar";
+import { useState } from "react";
+import { updateOrder } from "@/firebase/firestore";
+import { Timestamp } from "@firebase/firestore";
 
 type variant = 'default' | 'secondary' | 'outline' | 'complete' | 'processing' | 'picking' | 'issue' | null | undefined;
 
@@ -52,10 +57,36 @@ export function OrderCard({ order }: { order: Order }) {
         </div>
       </CardContent>
       <CardFooter className="flex justify-between">
-        <Button variant="destructive">Delete</Button>
-        <Button variant="outline">Schedule</Button>
         <Button variant="outline">Edit</Button>
+        <CalendarPopover workOrder={order.workOrder} />
       </CardFooter>
     </Card>
+  )
+}
+
+const CalendarPopover = ({workOrder}:{workOrder:string}) => {
+  const [date, setDate] = useState<Date>();
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+          <Button>Schedule</Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-4" align="start">
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          disabled={(date) =>
+            date > new Date() || date < new Date("1900-01-01")
+          }
+          initialFocus
+        />
+        <Button className="w-full" disabled={date === undefined} onClick={(e) => {
+          if (date) {
+            updateOrder(workOrder, { dates: { scheduled: true, build: Timestamp.fromDate(date) } })
+          }
+        }}>Confirm</Button>
+      </PopoverContent>
+    </Popover>
   )
 }
